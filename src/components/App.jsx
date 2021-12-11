7c758e3f2eb85c57aa15b13157936511ac4f64d3
Angel <angel.aven.c@gmail.com>
2021-12-11 00:16:19 -0600

    Initial configuration

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/global.scss'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hola mundo</h1>
+      <p>lorem</p>
+    </div>
+  )
+}
+
+export default App
