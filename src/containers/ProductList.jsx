017d948f442f2524ae729756c6e83438c87e7566
Angel <angel.aven.c@gmail.com>
2022-01-18 01:37:19 -0600

    Async/Await configuration
    Using axios

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import ProductItem from '../components/ProductItem'
 import '../styles/ProductList.scss'
+import axios from 'axios'
+
+const API = 'https://api.escuelajs.co/api/v1/products?limit=10&offset=1'
 
 const ProductList = () => {
+  const [products, setProducts] = useState([])
+  useEffect(async () => {
+    const response = await axios.get(API)
+    console.log(response)
+    setProducts(response.data)
+  }, [])
+
+
   return (
     <section className="main-container">
       <div className="ProductList">
-        <ProductItem />
+        {products.map(product => (
+          <ProductItem key={product.id} product={product}/>
+        ))}
       </div>
     </section>
   )
