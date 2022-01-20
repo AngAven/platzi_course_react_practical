b8fed8e74776622d2e5acb342d296e635b10e370
Angel <angel.aven.c@gmail.com>
2022-01-20 01:41:01 -0600

    Adding elements to state

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import AppContext from '@context/AppContext'
 
 import Menu from '@components/Menu'
 import menu from '@icons/icon_menu.svg'
+import MyOrder from '@containers/MyOrder'
 import shoppingCart from '@icons/icon_shopping_cart.svg'
 import logo from '@logos/logo_yard_sale.svg'
 import '@styles/Header.scss'
@@ -10,6 +11,7 @@ import '@styles/Header.scss'
 const Header = () => {
   const {state} = useContext(AppContext)
   const [toggle, setToggle] = useState(false)
+  const [toggleOrders, setToggleOrders] = useState(false)
   const handleToggle = () => setToggle(!toggle)
   return (
     <nav>
@@ -43,17 +45,21 @@ const Header = () => {
       <div className="navbar-right">
         <ul>
           <li className="navbar-email" onClick={handleToggle}>platzi@example.com</li>
-          <li className="navbar-shopping-cart">
+          <li className="navbar-shopping-cart" onClick={() => setToggleOrders(!toggleOrders)}>
             <img src={shoppingCart} alt="shopping cart"/>
-              {
-                state.cart.length > 0 ? <div>{state.cart.length}</div> : null
-              }
+            {
+              state.cart.length > 0 ? <div>{state.cart.length}</div> : null
+            }
           </li>
         </ul>
       </div>
 
       {
         toggle && <Menu/>
+      }
+
+      {
+        toggleOrders && <MyOrder/>
       }
 
     </nav>
